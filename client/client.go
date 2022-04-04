3ae4c732516f5d9ede3376bb0d6598a92d5497c4
cyzhzhd <cyzhzhd@gmail.com>
2022-04-04 22:20:04 +0900

    클라이언트가 메시지를 보내고 받을 수 있도록 변경

--- client/client.go
+++ client/client.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"strings"
 
 	"grpcChatServer/chatserver"
 
 	"google.golang.org/grpc"
 )
+
+type clientHandle struct {
+	stream     chatserver.Services_ChatServiceClient
+	clientName string
+}
 
 func main() {
 	serverUrl := "localhost:6000"
@@ -26,9 +34,57 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to call ChatService :: %v", err)
 	}
-	fmt.Println(stream)
 
-	// blocker
+	ch := clientHandle{stream: stream}
+	ch.clientConfig()
+	go ch.sendMessage()
+	go ch.receiveMessage()
+
 	blocker := make(chan bool)
 	<-blocker
+}
+
+func (ch *clientHandle) clientConfig() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("Your name : ")
+	name, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("Failed to read from console :: %v", err)
+	}
+	ch.clientName = strings.Trim(name, "\r\n")
+}
+
+func (ch *clientHandle) sendMessage() {
+	for {
+		reader := bufio.NewReader(os.Stdin)
+
+		clientMessage, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatalf("Failed to read from console :: %v", err)
+		}
+		clientMessage = strings.Trim(clientMessage, "\r\n")
+
+		clientMessageBox := &chatserver.FromClient{
+			Name: ch.clientName,
+			Body: clientMessage,
+		}
+
+		err = ch.stream.Send(clientMessageBox)
+
+		if err != nil {
+			log.Printf("Error while sending message to server :: %v", err)
+		}
+	}
+}
+
+func (ch *clientHandle) receiveMessage() {
+	for {
+		msg, err := ch.stream.Recv()
+		if err != nil {
+			log.Printf("Error in receiving message from server :: %v", err)
+		}
+
+		// print message to console
+		fmt.Printf("%s : %s \n", msg.Name, msg.Body)
+	}
 }
